e6ecb273eb767b18ec9cacfa7b897831ec2c1ea7
ajaykate-gslab <108253138+ajaykate-gslab@users.noreply.github.com>
2022-07-06 15:34:39 +0530

    Update My_KYC_App.java
    Taken input from user

--- KYC_App/src/B/My_KYC_App.java
+++ KYC_App/src/B/My_KYC_App.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class My_KYC_App extends KYC implements New_KYC {
 
 	@Override
-	public void apply_KYC(String adhar) {
-		System.out.println("KYC Done Using Adhar Number :"+adhar);
+	public void apply_KYC(BigInteger adhar) {
+		System.out.println("KYC Done Using Adhar Number :["+adhar+"]");
 		
 	}
 	
@@ -15,19 +15,29 @@ public class My_KYC_App extends KYC implements New_KYC {
 	
 		My_KYC_App k=new My_KYC_App();
 		Scanner sc =new Scanner(System.in);
-		
-		System.out.println("1. KYC Uding Old ID Proof(PAN and PassBook)");
-		System.out.println("2. KYC Uding New ID Proof (Adhar Number)");
+		System.out.println("********* WELCOME TO KYC CENTER *********");
+		System.out.println("------------------------------------------");
+		System.out.println("1. KYC Using Old ID Proof(PAN and PassBook)");
+		System.out.println("2. KYC Using New ID Proof (Adhar Number)");
 		System.out.println("Select your choice 1 or 2 :");
 		int n=sc.nextInt();
 		
 		switch (n) {
 		case 1: {
-			k.apply_KYC("hdj667676", "H68539");
+			int n1 =3;
+			 System.out.println("Enter PAN card Id and PassBook Id :"); 
+			 String[] docs =new String[5];
+			 for(int i=0; i<n1; i++) {
+				 docs[i]=sc.nextLine();
+			 }
+			 
+			k.apply_KYC(docs[1],docs[2]);
 			break;	
 		}
 		case 2: {
-			k.apply_KYC("648383483838");
+			System.out.println("Enter Your Adhar Card Number :");
+			BigInteger adhar=sc.nextBigInteger();
+			k.apply_KYC(adhar);
 			break;
 		}
 		default:
